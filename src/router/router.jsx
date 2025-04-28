5de91cddf8910220bcc45340c8d51ec2bd1509ee
tofiqsoftvance <tofiqulislam.softvance@gmail.com>
2025-04-28 18:16:11 +0600

    fixed issues

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,14 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/Layout";
 import HomePage from "../pages/HomePage";
 import DashboardLayout from "../layout/DashboardLayout";
-import Overview from "../pages/DashboardPages/Overview"
+import Overview from "../pages/DashboardPages/Overview";
 import Appeal from './../pages/DashboardPages/Appeal';
 import TicketManagement from './../pages/DashboardPages/TicketManagement';
 import ChatBot from './../pages/DashboardPages/ChatBot';
 import SubscriptionServices from './../pages/DashboardPages/SubscriptionServices';
 import Settings from './../pages/DashboardPages/Settings';
 import ErrorPage from './../pages/ErrorPage/ErrorPage';
-import ErrorPage from "../pages/ErrorPages/ErrorPage";
 import AboutUsPage from "../pages/AboutUsPage";
 import TermConditionPage from "../pages/TermConditionPage";
 import SubscriptionPlanPage from "../pages/SubscriptionPlanPage";
@@ -26,22 +25,10 @@ const router = createBrowserRouter([
     element: <Layout />,
     errorElement: <ErrorPage />,
     children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "about-us",
-        element: <AboutUsPage />,
-      },
-      {
-        path: "terms-condition",
-        element: <TermConditionPage />,
-      },
-      {
-        path: "subscription-plan",
-        element: <SubscriptionPlanPage />,
-      },
+      { path: "/", element: <HomePage /> },
+      { path: "about-us", element: <AboutUsPage /> },
+      { path: "terms-condition", element: <TermConditionPage /> },
+      { path: "subscription-plan", element: <SubscriptionPlanPage /> },
     ],
   },
 
@@ -51,54 +38,27 @@ const router = createBrowserRouter([
     element: <DashboardLayout />,
     errorElement: <ErrorPage />,
     children: [
-      {
-        path: 'overview',
-        element: <Overview />,
-      },
-      {
-        path: 'appeal',
-        element: <Appeal />,
-      },
-      {
-        path: 'ticket-management',
-        element: <TicketManagement />,
-      },
-      {
-        path: 'chat-bot',
-        element: <ChatBot />,
-      },
-      {
-        path: 'subscription-services',
-        element: <SubscriptionServices />,
-      },
-      {
-        path: 'settings',
-        element: <Settings />,
-      },
+      { path: "overview", element: <Overview /> },
+      { path: "appeal", element: <Appeal /> },
+      { path: "ticket-management", element: <TicketManagement /> },
+      { path: "chat-bot", element: <ChatBot /> },
+      { path: "subscription-services", element: <SubscriptionServices /> },
+      { path: "settings", element: <Settings /> },
     ],
+  },
+
+  // Auth Layout
   {
-    path:"/auth",
-    element:<AuthLayout />,
+    path: "/auth",
+    element: <AuthLayout />,
     errorElement: <ErrorPage />,
     children: [
-      {
-        path:"login",
-        element:<LoginPage />
-      },
-      {
-        path:"verify-otp",
-        element:<VerifyOtpPage />
-      },
-      {
-        path:"signup",
-        element:<SignupPage />
-      },
-      {
-        path:"terms-condition",
-        element:<TermsPage />
-      },
-    ]
-  }
+      { path: "login", element: <LoginPage /> },
+      { path: "verify-otp", element: <VerifyOtpPage /> },
+      { path: "signup", element: <SignupPage /> },
+      { path: "terms-condition", element: <TermsPage /> },
+    ],
+  },
 ]);
 
 export default router;
