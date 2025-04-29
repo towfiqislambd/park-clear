d111dcf713c517e2b6b7d0ccf7006fee85296157
tofiqsoftvance <tofiqulislam.softvance@gmail.com>
2025-04-29 14:50:00 +0600

    done

--- src/pages/DashboardPages/TicketManagement.jsx
+++ src/pages/DashboardPages/TicketManagement.jsx
@@ -1,10 +1,24 @@
-import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import ProceedWithParkClear from "../../components/dashboard-pages/dashboard-appeal/ProceedWithParkClear";
+import GenerateLetter from "../../components/dashboard-pages/dashboard-appeal/GenerateLetter";
+import DefaultTicketManagement from "@/components/dashboard-pages/dashbaord-ticketManagement/DefaultTicketManagement";
 
 const TicketManagement = () => {
+    const [proceed, setProceed] = useState(false)
+    const [appeal, setAppeal] = useState(false)
+
     return (
         <div>
-            TicketManagement
-            <Button>Click Here</Button>
+            {
+                proceed ?
+                    <ProceedWithParkClear />
+                    :
+                    appeal ?
+                        <GenerateLetter />
+                        :
+                        <DefaultTicketManagement setProceed={setProceed} setAppeal={setAppeal} />
+
+            }
         </div>
     );
 };
