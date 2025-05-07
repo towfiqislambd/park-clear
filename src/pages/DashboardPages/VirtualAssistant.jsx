b9024d5101f8e321d5ec495a00c735c5abc9194f
towfiqislambd <towfiqislambd@gmail.com>
2025-05-07 18:16:58 +0600

    done

--- src/pages/DashboardPages/VirtualAssistant.jsx
+++
@@ -1,9 +0,0 @@
-const VirtualAssistant = () => {
-    return (
-        <div>
-            <h2 className="text-3xl font-semibold text-theme-orange">Virtual Assistant....</h2>
-        </div>
-    );
-};
-
-export default VirtualAssistant;
